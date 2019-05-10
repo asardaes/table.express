004a217831b8189740f750eb545568ad39f813d2
asardaes <alexis.sarda@gmail.com>
2019-05-10 21:59:14 +0200

    Update mutate tests

--- tests/testthat/unit/test-mutate.R
+++ tests/testthat/unit/test-mutate.R
@@ -7,6 +7,11 @@ test_that("Mutating by reference without parsing works.", {
     expect_identical(ncol(ans), ncol(dt))
     expect_identical(dt$ans, dt$mpg * 2)
 
+    ans_name <- "ans"
+    ans <- dt %>% start_expr %>% mutate(!!ans_name := mpg * 2) %>% end_expr
+    expect_identical(ncol(ans), ncol(dt))
+    expect_identical(dt$ans, dt$mpg * 2)
+
     prev_ncol <- ncol(dt)
     dt %>% start_expr %>% mutate(ans = NULL) %>% end_expr
     expect_identical(ncol(dt), prev_ncol - 1L)
@@ -22,6 +27,11 @@ test_that("Mutating by reference with parsing works.", {
     dt <- data.table::copy(DT)
 
     ans <- dt %>% start_expr %>% mutate(ans = "mpg * 2", .parse = TRUE) %>% end_expr
+    expect_identical(ncol(ans), ncol(dt))
+    expect_identical(dt$ans, dt$mpg * 2)
+
+    ans_name <- "ans"
+    ans <- dt %>% start_expr %>% mutate(!!ans_name := "mpg * 2", .parse = TRUE) %>% end_expr
     expect_identical(ncol(ans), ncol(dt))
     expect_identical(dt$ans, dt$mpg * 2)
 
@@ -44,6 +54,11 @@ test_that("Mutating by value without parsing works.", {
     ans <- ans %>% start_expr %>% mutate(ans = NULL, .by_ref = FALSE) %>% end_expr
     expect_identical(ans, DT)
 
+    ans_name <- "ans"
+    ans <- DT %>% start_expr %>% mutate(!!ans_name := mpg * 2, .by_ref = FALSE) %>% end_expr
+    expect_identical(ncol(ans), ncol(DT) + 1L)
+    expect_identical(ans$ans, ans$mpg * 2)
+
     ans <- DT %>% start_expr %>% mutate(mpg2 = mpg * 2, disp0.5 = disp / 2, .by_ref = FALSE) %>% end_expr
     expect_identical(ncol(ans), ncol(DT) + 2L)
     expect_identical(ans$mpg2, DT$mpg * 2)
@@ -57,6 +72,11 @@ test_that("Mutating by value with parsing works.", {
 
     ans <- ans %>% start_expr %>% mutate(ans = "NULL", .parse = TRUE, .by_ref = FALSE) %>% end_expr
     expect_identical(ans, DT)
+
+    ans_name <- "ans"
+    ans <- DT %>% start_expr %>% mutate(!!ans_name := "mpg * 2", .parse = TRUE, .by_ref = FALSE) %>% end_expr
+    expect_identical(ncol(ans), ncol(DT) + 1L)
+    expect_identical(ans$ans, ans$mpg * 2)
 
     ans <- DT %>%
         start_expr %>%
