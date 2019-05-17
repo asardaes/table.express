12d596bf8d18065fb5087953fb56f8d144359b51
asardaes <alexis.sarda@gmail.com>
2019-05-17 23:14:05 +0200

    Add comment about rlang's evolution

--- R/pkg.R
+++ R/pkg.R
@@ -3,6 +3,9 @@
 #' A set of light-weight bindings that map [dplyr::dplyr-package] verbs to an expression that can be
 #' used within a [data.table::data.table-package]'s frame.
 #'
+#' Since this package's functionality is based on the \pkg{rlang} package, and it is still evolving,
+#' breaking changes may be needed in the future.
+#'
 #' @author Alexis Sarda-Espinosa
 #'
 #' @importFrom data.table :=
