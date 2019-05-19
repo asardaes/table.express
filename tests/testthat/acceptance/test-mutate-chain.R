b9affaae27b70f3db7959d10491e492a83c08b4a
asardaes <alexis.sarda@gmail.com>
2019-05-19 15:42:48 +0200

    Add another acceptance test

---
+++ tests/testthat/acceptance/test-mutate-chain.R
@@ -0,0 +1,24 @@
+context("  Mutation chain")
+
+test_that("Several mutations in one clause are equivalent to a chain of mutations.", {
+    expected <- data.table::copy(DT)
+    expected <- expected[, `:=`(foo = mpg - 3, bar = log(disp), baz = vs + am)]
+
+    dt <- data.table::copy(DT)
+    ans <- dt %>%
+        start_expr %>%
+        mutate(foo = mpg - 3, bar = log(disp), baz = vs + am) %>%
+        end_expr
+
+    expect_identical(ans, expected)
+
+    dt <- data.table::copy(DT)
+    ans <- dt %>%
+        start_expr %>%
+        mutate(foo = mpg - 3) %>%
+        mutate(bar = log(disp)) %>%
+        mutate(baz = vs + am) %>%
+        end_expr
+
+    expect_identical(ans, expected)
+})
--- tests/testthat/test-20-acceptance.R
+++ tests/testthat/test-20-acceptance.R
@@ -1,3 +1,4 @@
 context("Acceptance tests")
 
 source("acceptance/test-order-invariance.R", TRUE)
+source("acceptance/test-mutate-chain.R", TRUE)
