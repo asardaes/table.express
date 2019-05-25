5808deb918ced4100f0074e7cc8a399a5b253aad
asardaes <alexis.sarda@gmail.com>
2019-05-25 13:19:15 +0200

    Move tidyselect reexports to their own file

---
+++ R/UTILS-tidyselect.R
@@ -0,0 +1,39 @@
+#' @importFrom tidyselect starts_with
+#' @export
+#'
+tidyselect::starts_with
+
+#' @importFrom tidyselect ends_with
+#' @export
+#'
+tidyselect::ends_with
+
+#' @importFrom tidyselect contains
+#' @export
+#'
+tidyselect::contains
+
+#' @importFrom tidyselect matches
+#' @export
+#'
+tidyselect::matches
+
+#' @importFrom tidyselect num_range
+#' @export
+#'
+tidyselect::num_range
+
+#' @importFrom tidyselect one_of
+#' @export
+#'
+tidyselect::one_of
+
+#' @importFrom tidyselect everything
+#' @export
+#'
+tidyselect::everything
+
+#' @importFrom tidyselect last_col
+#' @export
+#'
+tidyselect::last_col
