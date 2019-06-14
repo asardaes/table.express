a924ae7eee3a41403296ff3e58911fec5fae0513
asardaes <alexis.sarda@gmail.com>
2019-06-14 20:26:36 +0200

    Add anti_join

---
+++ R/VERBS-anti_join.R
@@ -0,0 +1,26 @@
+#' @importFrom dplyr anti_join
+#' @export
+#'
+dplyr::anti_join
+
+#' @include VERBS-joins.R
+#' @rdname joins
+#' @export
+#' @importFrom rlang enexpr
+#' @importFrom rlang enexprs
+#' @importFrom rlang expr
+#'
+#' @examples
+#'
+#' rhs %>%
+#'     start_expr %>%
+#'     right_join(lhs, x, v) %>%
+#'     end_expr
+#'
+anti_join.ExprBuilder <- function(x, y, ...) {
+    y <- rlang::expr(`!`(`!!`(rlang::enexpr(y))))
+    on <- lapply(rlang::enexprs(...), to_expr, .parse = TRUE)
+
+    x$set_where(y, TRUE)
+    frame_append(x, on = list(!!!on))
+}
---
+++ tests/testthat/unit/test-anti_join.R
@@ -0,0 +1,11 @@
+context("  anti_join")
+
+test_that("Anti join works.", {
+    expected <- rhs[!lhs, on = "x"]
+    ans <- rhs %>% start_expr %>% anti_join(lhs, "x") %>% end_expr
+    expect_identical(ans, expected)
+
+    expected <- paypal[!website, on = .(payment_id = session_id)]
+    ans <- paypal %>% start_expr %>% anti_join(website, payment_id = session_id) %>% end_expr
+    expect_identical(ans, expected)
+})
