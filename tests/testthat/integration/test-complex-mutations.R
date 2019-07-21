7c0ee73f7a547ac259ac9ddd7d75cfb05b3bc76d
asardaes <alexis.sarda@gmail.com>
2019-07-21 16:26:43 +0200

    Add more eager integration tests

--- tests/testthat/integration/test-complex-mutations.R
+++ tests/testthat/integration/test-complex-mutations.R
@@ -18,6 +18,12 @@ test_that("Mutating a subset of rows works.", {
         end_expr
 
     expect_identical(ans, expected)
+
+    ans <- data.table::copy(state) %>%
+        where(grepl("^N", region), grepl("^N", name)) %>%
+        mutate(abb = paste("N", abb, sep = "-"))
+
+    expect_identical(ans, expected)
 })
 
 test_that("Mutating by group works.", {
@@ -28,6 +34,12 @@ test_that("Mutating by group works.", {
         mutate(abb = paste(.GRP, abb, sep = "-")) %>%
         group_by(region, division) %>%
         end_expr
+
+    expect_identical(ans, expected)
+
+    ans <- data.table::copy(state) %>%
+        group_by(region, division) %>%
+        mutate(abb = paste(.GRP, abb, sep = "-"))
 
     expect_identical(ans, expected)
 })
@@ -41,6 +53,14 @@ test_that("Mutating subset by group works.", {
         where(area > 50000) %>%
         group_by(region, division) %>%
         end_expr(.by_ref = FALSE)
+
+    expect_identical(ans, expected)
+    expect_false(identical(ans, state))
+
+    ans <- data.table::copy(state) %>%
+        group_by(region, division) %>%
+        where(area > 50000) %>%
+        mutate(abb = paste(.GRP, abb, sep = "-"))
 
     expect_identical(ans, expected)
     expect_false(identical(ans, state))
@@ -59,6 +79,16 @@ test_that("Mutating subset by group works.", {
         mutate(abb = paste(0, abb, sep = "-")) %>%
         where(area <= 50000) %>%
         end_expr
+
+    expect_identical(ans, expected)
+    expect_false(identical(ans, state))
+
+    ans <- data.table::copy(state) %>%
+        group_by(region, division) %>%
+        where(area > 50000) %>%
+        mutate(abb = paste(.GRP, abb, sep = "-")) %>%
+        where(area <= 50000) %>%
+        mutate(abb = paste(0, abb, sep = "-"))
 
     expect_identical(ans, expected)
     expect_false(identical(ans, state))
