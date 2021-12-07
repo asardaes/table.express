934e36c9f7aceba6307c0591a39fb57d0831ccad
asardaes <alexis.sarda@gmail.com>
2021-12-07 01:30:51 +0100

    Update note about delegation warning

--- R/UTILS-misc.R
+++ R/UTILS-misc.R
@@ -319,7 +319,8 @@ try_delegate <- function(.fun_, .expr, .generic_env = rlang::caller_env()) {
             if (isTRUE(getOption("table.express.warn.cedta", TRUE))) {
                 rlang::warn(paste(err$message,
                                   "Trying to dispatch to data.frame method (allowing copies).",
-                                  "Use options(table.express.warn.cedta = FALSE) to avoid this warning."))
+                                  "Use options(table.express.warn.cedta = FALSE) to avoid this warning",
+                                  "and check the package documentation for more information."))
             }
 
             do.call(NextMethod, list(.fun_), envir = .generic_env)
--- R/pkg.R
+++ R/pkg.R
@@ -16,9 +16,12 @@
 #' Note that since version 0.3.0, it is not possible to load \pkg{table.express} and \pkg{dtplyr} at
 #' the same time, since they define the same `data.table` methods for many \pkg{dplyr} generics.
 #'
-#' If a package uses `dplyr` without importing `data.table`, the methods in this package will try to
-#' delegate to the `data.frame` methods with a warning. To avoid the warning, use
-#' `options(table.express.warn.cedta = FALSE)`.
+#' Bearing in mind that `data.table`s are also `data.frame`s, we have to consider that other
+#' packages may uses `dplyr` internally without importing `data.table`. Since `dplyr`'s methods are
+#' generic, calls to these methods in such packages would fail. The functions in this package try to
+#' detect when this happens and delegate to the `data.frame` methods with a warning, which can be
+#' safely ignored if you know that the error originates from a package that is not meant to work
+#' with `data.table`. To avoid the warning, use `options(table.express.warn.cedta = FALSE)`.
 #'
 #' This software package was developed independently of any organization or institution that is or
 #' has been associated with the author.
